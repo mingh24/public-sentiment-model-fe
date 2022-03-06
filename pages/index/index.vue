765e528cb84e523a6be416b5b86ec860a3f75d14
mingh24 <1275432232@qq.com>
2022-03-06 16:27:10 +0800

    feat: 默认页面

---
+++ pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<view class="container">
+		<u-empty mode="permission"></u-empty>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.container {
+		background-color: #FFFFFF;
+	}
+</style>
