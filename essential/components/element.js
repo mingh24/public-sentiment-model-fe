86d7b3e71e2c85d6231fc8bf50c18634b4bd4aa3
mingh24 <1275432232@qq.com>
2022-04-26 23:15:50 +0800

    fix: 解决Element Select输入时iOS无法弹出键盘的问题

--- essential/components/element.js
+++ essential/components/element.js
@@ -3,6 +3,14 @@ import Slider from 'element-ui/lib/slider'
 import Select from 'element-ui/lib/select'
 import Option from 'element-ui/lib/option'
 
+// 解决移动端无法正常弹出键盘
+// @see https://github.com/ElemeFE/element/issues/12563
+Select.computed.readonly = function () {
+    // trade-off for IE input readonly problem: https://github.com/ElemeFE/element/issues/10403
+    const isIE = !this.$isServer && !isNaN(Number(document.documentMode))
+    return !this.filterable || this.multiple || (!isIE && !this.visible && !this.remote)
+}
+
 const element = {
     install: function (Vue) {
         Vue.use(Slider)
